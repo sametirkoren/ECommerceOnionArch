a99ce7c70fa2c70026c8fd5473fb6e9cc76cd408
samet irkören <sametirkoren@gmail.com>
2022-10-29 13:56:29 +0300

    - ECommerce Onion Architecture Preparation finished

---
+++ Core/ECommerce.Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerce.Domain.Entities.Common;
+
+namespace ECommerce.Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public int Stock { get; set; }
+
+        public long Price { get; set; }
+    }
+}
---
+++ Infrastructure/ECommerce.Persistence/ServiceRegistration.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.Application.Abstractions;
+using ECommerce.Persistence.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ECommerce.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+        }
+    }
+}
